06911fb46bb20150621453f88b2de960d8de1db4
LegendBot <LegendBot@hotmail.com>
2015-03-29 14:44:12 +0100

    Working for patch 5.6 (2015-03-29)

--- AntiAfk.lua
+++ AntiAfk.lua
@@ -3,16 +3,21 @@ function _AntiAfk:__init()
 	self.Clock = os.clock()
 	AddTickCallback(function()
 		if os.clock() < self.Clock then return end
+		self.Clock = os.clock() + math.random(60,120)
 		myHero:MoveTo(myHero.x,myHero.z)
 	end)
-	AddSendPacketCallback(function(p)
-		if p.header == 0x00BF then
-			self.Clock = os.clock() + math.random(60,170)
+	AddRecvPacketCallback(function(p)
+		if p.header == 0x0027 then
+			p.pos = 2
+			local nID = p:DecodeF()
+			self.Clock = os.clock() + math.random(60,120)
 		end
 	end)
 end
 
 AddLoadCallback(function()
-	print("Loaded Anit-Afk")
+	HookPackets()
+	print("Loaded Anit-Afk.")
+	print("Please remember that you can still be reported and banned.")
 	_AntiAfk()
 end)
