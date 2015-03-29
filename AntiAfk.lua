c5b97fa87de8eb4542501f6ce6eb9b3ef96b29aa
LegendBot <LegendBot@hotmail.com>
2015-03-29 15:05:20 +0100

    Working for patch 5.6 (2015-03-29)

--- AntiAfk.lua
+++ AntiAfk.lua
@@ -1,23 +1,18 @@
 class '_AntiAfk'
 function _AntiAfk:__init()
 	self.Clock = os.clock()
+	self.Menu = scriptConfig("[Anti-Afk]","AntiAfk")
+	self.Menu:addParam("Enabled","Enable Script",1,false)
 	AddTickCallback(function()
-		if os.clock() < self.Clock then return end
+		if os.clock() < self.Clock or not self.Menu.Enabled then return end
 		self.Clock = os.clock() + math.random(60,120)
 		myHero:MoveTo(myHero.x,myHero.z)
-	end)
-	AddRecvPacketCallback(function(p)
-		if p.header == 0x0027 then
-			p.pos = 2
-			local nID = p:DecodeF()
-			self.Clock = os.clock() + math.random(60,120)
-		end
+		print("Anti-Afk: Moving")
 	end)
 end
 
 AddLoadCallback(function()
-	HookPackets()
-	print("Loaded Anit-Afk.")
-	print("Please remember that you can still be reported and banned.")
+	print("Anti-Afk: Loaded.")
+	print("Anti-Afk: Please remember that you can still be reported and banned.")
 	_AntiAfk()
 end)
