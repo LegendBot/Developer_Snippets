8187926588286cc3fe1b88e66a8a8eae070b7921
LegendBot <LegendBot@hotmail.com>
2015-04-08 22:00:33 +0100

    Working for patch 5.7 - 2015-04-08

--- Auto_Level.lua
+++ Auto_Level.lua
@@ -41,18 +41,18 @@ function AutoLevel:__init(table)
 end
 
 function AutoLevel:LevelSpell(ID)
-	local offsets = {[_Q] = 0x7F, [_W] = 0x5B, [_E] = 0x5F, [_R] = 0x6B}
-	local p = CLoLPacket(0x012A)
-	p.vTable = 0xE912D0
+	local offsets = {[_Q] = 0x66, [_W] = 0x65, [_E] = 0x64, [_R] = 0x63,}
+	local p = CLoLPacket(0x0017)
+	p.vTable = 0xE90950
 	p:EncodeF(myHero.networkID)
-	p:Encode4(0xA7A7A7A7)
-	p:Encode1(0xCA)
+	p:Encode4(0xC7C7C7C7)
 	p:Encode1(offsets[id])
-	p:Encode4(0xCACACACA)
+	p:Encode1(0x02)
+	p:Encode4(0xA9A9A9A9)
+	p:Encode4(0xD3D3D3D3)
 	p:Encode4(0x00000000)
-	p:Encode1(0x00) --Game specific byte
-	p:Encode1(0x00) --Game specific byte
-	p:Encode1(0x00) --Game specific byte
-	p:Encode2(0x0000)
+	p:Encode1(0x00)
 	SendPacket(p)
 end
+
+--[[Updated for 5.7 - 2015-04-08]]
