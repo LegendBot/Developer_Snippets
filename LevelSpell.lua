37dc9256444eaf980c1c89c1bf94f7228f361ffb
LegendBot <LegendBot@hotmail.com>
2015-09-16 17:28:25 +0100

    Updated for 5.18 - 2015-09-16

--- LevelSpell.lua
+++ LevelSpell.lua
@@ -7,20 +7,20 @@ print("LevelSpell override is active")
 
 _G.LevelSpell = function(id)
 	local offsets = {
-		[_Q] = 0x7D,
-		[_W] = 0xFD,
-		[_E] = 0x8B,
-		[_R] = 0x97,
+		[_Q] = 0xFB,
+		[_W] = 0xEB,
+		[_E] = 0xDB,
+		[_R] = 0xCB,
 	}
-	local p = CLoLPacket(0x0142)
-	p.vTable = 0xEBFE44
+	local p = CLoLPacket(0x10A)
+	p.vTable = 0xEDD384
 	p:EncodeF(myHero.networkID)
-	p:Encode1(0x2A)
-	for i = 1, 4 do	p:Encode1(0x12)	end
+	for i = 1, 4 do	p:Encode1(0x13)	end
+	p:Encode1(0x9E)
 	p:Encode1(offsets[id])
-	for i = 1, 4 do	p:Encode1(0x5E)	end
-	for i = 1, 4 do	p:Encode1(0xBD)	end
+	for i = 1, 4 do	p:Encode1(0x81)	end
+	for i = 1, 4 do	p:Encode1(0x57)	end
 	for i = 1, 4 do	p:Encode1(0x00)	end
 	SendPacket(p)
 end
---[[Updated for 5.17 - 2015-09-04]]
+--[[Updated for 5.18 - 2015-09-16]]
